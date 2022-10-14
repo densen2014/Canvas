45c030f982a0d32ef0f9696c03d7adace3df36bb
Alex Chow <zhouchuanglin@gmail.com>
2022-10-14 17:29:06 +0200

    net6

--- src/Blazor.Extensions.Canvas/BECanvasComponent.cs
+++ src/Blazor.Extensions.Canvas/BECanvasComponent.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
 
 namespace Blazor.Extensions
 {
-    public class BECanvasComponent : ComponentBase
+    public class BECanvasComponent : ComponentBase, IAsyncDisposable
     {
         [Parameter]
         public long Height { get; set; }
@@ -19,5 +21,23 @@ namespace Blazor.Extensions
 
         [Inject]
         internal IJSRuntime JSRuntime { get; set; }
+
+        private IJSObjectReference _module;
+        
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                this._module = await this.JSRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Blazor.Extensions.Canvas/blazor.extensions.canvas.js");
+            }
+        }
+        async ValueTask IAsyncDisposable.DisposeAsync()
+        {
+            if (this._module is not null)
+            {
+                await this._module.DisposeAsync();
+            }
+        }
+
     }
 }
